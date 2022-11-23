fe6a1b01b2d0b7a8db1ce701333c1d3d9774981b
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-23 16:08:43 +0530

    Added:Return Minimum Fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -5,11 +5,9 @@ public class CabInvoiceGenerator {
     static final  double COST_PER_MINUTE=1;
     static final  double MINIMUM_COST=5;
 
-
-    public double calculateTotalFair(double distans, double time) {
-        double distance = 0;
+    public double calculateTotalFair(double distance, double time) {
         double cost =distance*COST_PER_KM+time*COST_PER_MINUTE;
-        return cost;
+        return cost<MINIMUM_COST ? MINIMUM_COST : cost;
     }
 }
 
