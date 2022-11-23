5f9804302815c62199d5fcd92afd86b9c7322e55
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-23 16:02:15 +0530

    Added : Calculate Fair Added

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,8 +1,17 @@
 package com.bridgelabz;
 
 public class CabInvoiceGenerator {
+    static final  double COST_PER_KM=10;
+    static final  double COST_PER_MINUTE=1;
+    static final  double MINIMUM_COST=5;
 
-    public double calculateTotalFair(int i, int i1) {
-        return 0;
+
+    public double calculateTotalFair(double distans, double time) {
+        double distance = 0;
+        double cost =distance*COST_PER_KM+time*COST_PER_MINUTE;
+        return cost;
     }
 }
+
+
+
